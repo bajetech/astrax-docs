436f6498ed5bf6d60242f5e1c2678d49396cbbdc
Owan Hunte <owanh@hotmail.com>
2021-09-02 08:02:35 -0400

    feat: add 'Playground' section to the docs
    Allows you to test your connection with the extension.

---
+++ docs/playground/components/IsConnectedDemo.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { isConnected } from "@bajetech/astrax-api";
+import { PlaygroundInput } from "./basics/inputs";
+
+export const IsConnectedDemo = () => {
+  const [isConnectedState, setIsConnectedState] = useState(" ");
+  const btnHandler = () => {
+    setIsConnectedState(isConnected().toString());
+  };
+  return (
+    <section>
+      <div>
+        Is AstraX currently connected to your browser?
+        <PlaygroundInput readOnly value={isConnectedState} />
+      </div>
+      <button type="button" onClick={btnHandler}>
+        Check Connection
+      </button>
+    </section>
+  );
+};
